6c4332aebfb709599a3caef1a516f18ad5169352
orezomi <harismoaji@gmail.com>
2016-05-11 10:28:34 +0700

    Create imagoz.php

---
+++ imagoz.php
@@ -0,0 +1,25 @@
+<?php
+namespace orezomi\image;
+/**
+ * harismo aji
+ * harismoaji@gmail.com
+ */
+use Yii;
+use yii\base\Component;
+use yii\base\ErrorException;
+/**
+ * Class ImageDriver
+ * The main class to wrap Zend Feed Extension
+ * @package yii\feed
+ */
+class Imagoz extends Component
+{
+        /**
+         * Loads read Zend-feed component
+         * @return mixed object Zend\Feed\Reader component
+         */
+        public function image(){
+            return true;
+        }
+}
+?>
